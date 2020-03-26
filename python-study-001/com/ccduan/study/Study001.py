68a8b0e1e018f04b05f85ce01c7e5c461855ff05
ccDuan <577815733@qq.com>
2020-03-26 22:15:10 +0800

    python 学习基础代码

---
+++ python-study-001/com/ccduan/study/Study001.py
@@ -0,0 +1,17 @@
+#python的注释
+import time;#导入时间模块
+print("hello word")
+
+if 10-9>0:
+    #这里是缩进，代表四个空格
+    print("if内语句")
+
+#type函数判断数据类型，整数int  浮点数float， 字符串str， 布尔类型bool
+type("字符串")
+type(8.8)
+
+#数据类型转换
+int('8')
+str(123)
+bool(123) #true
+bool(0)# false
---
+++ python-study-001/com/ccduan/study/study002_jisuan.py
@@ -0,0 +1,8 @@
+#python的计算
+print(100/8)
+
+#变量
+
+a=100
+b=8
+print(a/b)
